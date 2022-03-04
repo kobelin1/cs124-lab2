bb93275e5f00f70847f3a787de79f487682c51e6
RomanHerrera1 <rherrera@hmc.edu>
2022-03-03 18:06:27 -0800

    Added craete item text box, fixed some visual issues, and started renaming

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {useState} from "react";
 import {generateUniqueID} from "web-vitals/dist/modules/lib/generateUniqueID";
@@ -22,22 +22,28 @@ const data = [
 
 function App() {
     const [listItems, setListItems] = useState(data)
-    const initalSelected = listItems.filter(item => item.checked)
+    const initialSelected = listItems.filter(item => item.checked)
     const initialSelectedIds = []
-    initalSelected.map(item => initialSelectedIds.push(item.id))
+    initialSelected.map(item => initialSelectedIds.push(item.id))
     const [selectedItems, setSelectedItems] = useState(initialSelectedIds)
     const [hideCompleted, setHideCompleted] = useState(false)
-    console.log("selectedItems: ", selectedItems)
+
+    const [curText, setCurText] = useState("")
+    const [addingItem, setAddingItem] = useState(false)
+
+    const [renaming, setRenaming] = useState(false)
+    const [textBoxList, setTextBoxList] = useState([])
+
+    const [areYouSure, setAreYouSure] = useState(true)
 
     function TaskList(){
         return(<div>
-                {/*<table>*/}
-                {/*    <tbody>*/}
-                {hideCompleted ? listItems.map((item) => (item.checked === false) && <Item id={item.id} name={item.name} value={item.value} checked={item.checked} text={item.text}/>) : listItems.map((item) => <Item id={item.id} name={item.name} value={item.value} checked={item.checked} text={item.text}/>)}
-                {/*    </tbody>*/}
-                {/*</table>*/}
+
+            {!renaming ? (hideCompleted ? listItems.map((item) => (item.checked === false) && <Item id={item.id} key={item.id} name={item.name} value={item.value} checked={item.checked} text={item.text}/>) : listItems.map((item) => <Item id={item.id} name={item.name} value={item.value} checked={item.checked} text={item.text}/>)) : false}
+            {!addingItem && !renaming && <button type={"button"} id="item_button" name="item_button" onClick={() => setAddingItem(true)}>Create New Item +</button>}
+            {renaming && textBoxList.map((item) => <span><input type="text" id={item.id} name={item.id} value={item.text} onChange={e => setTextBoxList(textBoxList.map(item => item.id === e.target.id ? item.text = e.target.value: false))}/><br/></span>)}
+
                 {/*<button type="button" id="item_button" name="item_button">Create new item +</button>*/}
-                <button type={"button"} id="item_button" name="item_button" onClick={handleAddItemClick}>Create New Item +</button>
                 {/*{selectedItems.length > 0 &&*/}
                 {/*    <button type={"button"} onClick={handleDeleteClick}>Delete Selected</button>}*/}
                 {/*{listItems.length > 0 &&*/}
@@ -48,20 +54,33 @@ function App() {
 
     function handleAddItemClick(){
         console.log("add item click")
+
+        return (<span>
+            <label htmlFor = "item_enter"> Enter new item text:</label>
+            <br/>
+            <input type="text" id="item_enter" name="item_enter" value={curText} onChange={event => {setCurText(curText + event)}}/>
+            <button type="button" id="add_item" name="add_item" onClick={handleNewItem}>Add</button>
+        </span>);
+    }
+
+    function handleNewItem(){
+        const uniqueId = generateUniqueID()
         setListItems([...listItems,
             {
-                id: generateUniqueID(),
-                name: "name",
-                value: "value",
-                text: generateUniqueID(),
+                id: uniqueId,
+                name: uniqueId,
+                value: uniqueId,
+                text: curText,
                 checked: false
             }]);
+        setCurText("")
+        setAddingItem(false)
     }
 
     function handleDeleteClick(){
         setListItems(listItems.filter(i => !selectedItems.includes(i.id)));
         setSelectedItems([]);
-        console.log(selectedItems);
+        setAreYouSure(true);
     }
 
     function handleRenameClick(listId, field, value){
@@ -70,12 +89,19 @@ function App() {
             i => i.id === listId ? {...i, [field]: value} : i))
     }
 
-    function handleShowClick(){
+    function handleStartRenaming(){
+        setRenaming(true)
+        const textList = []
+        listItems.map(item => textList.push({id: item.id, text: item.text}))
+        setTextBoxList(textList)
+    }
+
+    function handleEndRenaming(){
 
     }
 
-    function handleToggleItemSelect(itemId, itemText){
-        console.log(itemText)
+
+    function handleToggleItemSelect(itemId){
         listItems.map(item => (item.id === itemId) ? (item.checked = !item.checked) : false);
         if (selectedItems.includes(itemId)) {
             setSelectedItems(selectedItems.filter(i => i !== itemId));
@@ -93,18 +119,21 @@ function App() {
         return (<div className="list">
                 <br />
 
-                {listItems.length > 0 && <button type="button" id="rename_button" name="rename_button">Rename</button>}
-                {!hideCompleted && <button type="button" id="show_uncompleted_button" name="show_uncompleted_button" onClick={() => setHideCompleted(true)}>Hide Completed</button>}
-                {hideCompleted && <button type="button" id="show_uncompleted_button" name="show_uncompleted_button" onClick={() => setHideCompleted(false)}>Show All Items</button>}
-                {selectedItems.length > 0 && <button type="button" id="delete_button" name="delete_button" onClick={handleDeleteClick}>Delete Selected</button>}
+                {!addingItem && !renaming && listItems.length > 0 && <button type="button" id="rename_button" name="rename_button" onClick={() => handleStartRenaming}>Rename</button>}
+                {!addingItem && renaming && listItems.length > 0 && <button type="button" id="rename_button" name="rename_button" onClick={() => handleEndRenaming}>Finish Renaming</button>}
 
+                {(!hideCompleted && !renaming && listItems.length > 0 && !addingItem)  && <button type="button" id="show_uncompleted_button" name="show_uncompleted_button" onClick={() => setHideCompleted(true)}>Hide Completed</button>}
+                {(hideCompleted && !renaming && listItems.length > 0 && !addingItem) && <button type="button" id="show_uncompleted_button" name="show_uncompleted_button" onClick={() => setHideCompleted(false)}>Show All Items</button>}
+
+                {selectedItems.length > 0 && !renaming && !addingItem && areYouSure && <button type="button" id="delete_button" name="delete_button" onClick={() => setAreYouSure(false)}>Delete Selected</button>}
+                {selectedItems.length > 0 && !renaming && !addingItem && !areYouSure &&<button type="button" id="delete_button" name="delete_button" onClick={handleDeleteClick}>Confirm Deletions</button>}
             </div>
         );
     }
 
     function Item(props){
         return (<span>
-            <input type="checkbox" id={props.id} name={props.name} value={props.value} checked={props.checked} className="bigCheckbox" onChange={() => handleToggleItemSelect(props.id, props.text)} />
+            <input type="checkbox" id={props.id} name={props.name} value={props.value} checked={props.checked} className="bigCheckbox" onChange={() => handleToggleItemSelect(props.id)} />
             <label htmlFor={props.id}>{props.text}</label><br/><br/>
             </span>
         );
@@ -117,6 +146,12 @@ function App() {
       </div>
         {listItems.length === 0 && <i>There is nothing in your list!</i>}
         {TaskList()}
+
+        {addingItem && !renaming && <span>
+        <label htmlFor = "item_enter"> Enter new item text:</label>
+        <br/>
+        <input type="text" id="item_enter" name="item_enter" value={curText} onChange={event => {setCurText(event.target.value)}}/>
+        <button type="button" id="add_item" name="add_item" onClick={handleNewItem}>Add</button></span>}
         {ButtonList()}
     </div>
 
